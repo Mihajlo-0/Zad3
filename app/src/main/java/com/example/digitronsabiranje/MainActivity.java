4369a0ed1c1048bb102bd39e354fcc95de7aa4ab
Mihajlo-0 <mihajlo.djordjevicc1@gmail.com>
2024-05-24 13:01:01 +0200

    gotov zadatak

--- app/src/main/java/com/example/digitronsabiranje/MainActivity.java
+++ app/src/main/java/com/example/digitronsabiranje/MainActivity.java
@@ -3,20 +3,25 @@ package com.example.digitronsabiranje;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText editBroj1,editBroj2;
+    TextView rezultat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         editBroj1=(EditText) findViewById(R.id.editTextNumber);
         editBroj2=(EditText) findViewById(R.id.editTextNumber2);
+        rezultat=(TextView) findViewById(R.id.rezultatText);
     }
     public void onClickSaberi(View view){
-        double rez=Double.parseDouble(editBroj1.getText()))+Double.parseDouble(editBroj2.getText());
+        double rez=Double.parseDouble(String.valueOf(editBroj1.getText()))+Double.parseDouble(String.valueOf(editBroj2.getText()));
+        rezultat.setText(String.valueOf(rez));
 
     }
 }
